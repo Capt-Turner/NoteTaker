19e86e3bde0cd6e73613ca671dd232e5f7fbccd5
Seth Turner <sethhet@gmail.com>
2022-11-21 21:13:42 -0700

    added get notes + get * to htmlroutes

---
+++ db/store.js
@@ -0,0 +1,12 @@
+const fs=require('fs');
+
+class Store{
+    read(){
+        return 
+    }
+
+    write(note){
+        return
+    }
+
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router=require('express').Router();
+const store=require('../db/store.js');
 
 router.get('/notes',(req,res)=>{
 
