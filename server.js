e783c88b1d2d9f0ca0253c0da8fb465dbffb188e
Seth Turner <sethhet@gmail.com>
2022-11-21 21:02:34 -0700

    added apiroutes initial code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const apiRoutes=require('./routes/apiRoutes');
+const apiRoutes=require('./routes/apiRoutes.js');
 const userRoutes=require('./routes/userRoutes');
 
 const app=express();
